2e0c23db22d9f1d88fb4bdbba391bf571ae3ea31
Choong Jin Ng <jin8383@gmail.ca>
2020-12-02 20:33:16 -0800

    Begun implementing ESPNOW and WiFi functionality. Updated BOM.ini

--- src/esp32_sensor_module/esp32_sensor_module.ino
+++ src/esp32_sensor_module/esp32_sensor_module.ino
@@ -1,9 +1,16 @@
 /**
    ESP32 Sensor Module
 
+   ESP32 Datasheet: https://www.espressif.com/sites/default/files/documentation/esp32-wroom-32_datasheet_en.pdf
+   IMU Library Reference: https://github.com/arduino-libraries/Arduino_LSM6DS3/blob/master/src/LSM6DS3.cpp
+   IMU Datasheet: https://www.st.com/content/ccc/resource/technical/document/datasheet/76/27/cf/88/c5/03/42/6b/DM00218116.pdf/files/DM00218116.pdf/jcr:content/translations/en.DM00218116.pdf
+   ESP-NOW and WiFi:
+    https://randomnerdtutorials.com/esp-now-two-way-communication-esp32/
+    https://randomnerdtutorials.com/esp-now-esp32-arduino-ide/
+    https://gist.github.com/futechiot/ee0223dd269cbe7d8605ce97d120d7d2
 
-   Reference: https://github.com/arduino-libraries/Arduino_LSM6DS3/blob/master/src/LSM6DS3.cpp
 */
+
 
 /**
    Metadata
@@ -14,7 +21,9 @@
 /**
    Libraries
 */
+#include "WiFi.h"
 #include "Wire.h" // ESP32 Wire.h
+#include <esp_now.h>
 
 /**
    ESP32 Pins
@@ -24,6 +33,7 @@
 #define IMU_SCL       14 // Pin 13
 #define IMU_SDA       12 // Pin 14
 //#define IMU_SD0       // Not implemented
+
 
 /**
    LSM Registers
@@ -58,13 +68,39 @@
 #define LSM6DSM_OUTZ_H_XL           0X2D
 
 /**
+   Structures
+*/
+
+//
+typedef struct canTechMsg {
+  //  char[10] sensorModuleMACAddress;
+
+  float accX;
+  float accY;
+  float accZ;
+
+  float gyroX;
+  float gyroY;
+  float gyroZ;
+
+  float temperature;
+};
+
+/**
    Constants
 */
-#define SERIAL_BAUD_RATE          115200
+#define SERIAL_BAUD_RATE            115200
+#define ESP32_WIFI_MODE             WIFI_STA
+
+//const char* READER_WIFI_SSD         "CANnect_Reader_WiFi_0.1"   // SSID of reader WiFi
+//const char* READER_WIFI_PASSWORD    "redboats"                  // Password of reader WiFi
 
 /**
    Variables
 */
+//uint8_t selfMACAddress[];
+//uint8_t readerMACAddress[];
+//esp_now_peer_info_t readerInfo;
 
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
@@ -79,12 +115,14 @@ void setup() {
   }
 
   imuSetup();
+  wifiSetup();
+  espNOWSetup();
 }
 
 void imuSetup(void) {
   Wire.begin(IMU_SDA, IMU_SCL);
 
-  while (readRegister(LSM6DSM_WHO_AM_I_REG) != 0x6A) {
+  while (readRegister(LSM6DSM_WHO_AM_I_REG) != WHO_AM_I_REG_VALUE) {
     Serial.println("Unable to talk to IMU! Trying again!");
     delay(1000);
   }
@@ -105,7 +143,19 @@ void imuSetup(void) {
   // Set the ODR config register to ODR/4
   writeRegister(LSM6DSM_CTRL8_XL, 0xA4); // I think this is correct??
 
-  Serial.println("Ready");
+  Serial.println("IMU ready");
+}
+
+void wifiSetup(void) {
+  WiFi.mode(ESP32_WIFI_MODE);
+  Serial.println(WiFi.macAddress());
+}
+
+void espNOWSetup(void) {
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
 }
 
 int writeRegister(uint8_t address, uint8_t value)
@@ -183,10 +233,8 @@ int readGyroscope(float& x, float& y, float& z) {
   return 1;
 }
 
-/**
-   Needs work
-*/
 int readAcceleration(float& x, float& y, float& z) {
+
   int16_t data[3];
 
   if (!readRegisters(LSM6DSM_OUTX_L_XL, (uint8_t*)data, sizeof(data))) {
@@ -204,9 +252,68 @@ int readAcceleration(float& x, float& y, float& z) {
   return 1;
 }
 
+char* convert_int16_to_str(int i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  char tmp_str[7];
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+//// Callback when data is sent
+//void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+//  Serial.print("\r\nLast Packet Send Status:\t");
+//  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+//  if (status == 0) {
+//    success = "Delivery Success :)";
+//  }
+//  else {
+//    success = "Delivery Fail :(";
+//  }
+//}
+
+//void connectToReader() {
+//  WiFi.begin(READER_WIFI_SSD, READER_WIFI_PASSWORD);
+//
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(500);
+//    Serial.println("Attempting to connect to WiFi");
+//  }
+//}
+
+/**
+   Register and add reader
+*/
+//void espNOWAddReader(uint8_t broadcastAddress) {
+//  if (esp_now_is_peer_exist(broadcastAddress)) {
+//    memcpy(readerInfo.peer_addr, broadcastAddress, 6);
+//    readerInfo.channel = 0;
+//    readerInfo.encrypt = false;
+//
+//    // Add peer
+//    if (esp_now_add_peer(&readerInfo) != ESP_OK) {
+//      Serial.println("Failed to add peer");
+//      return;
+//    }
+//  }
+//
+//  else {
+//    Serial.println("Unexpected behaviour!");
+//    return;
+//  }
+//}
+
+/**
+   Remove reader
+*/
+//void espNOWRemoveReader(void) {
+//  if (esp_now_del_peer(&readerInfo) != ESP_OK) {
+//    Serial.println("Failed to delete peer gracefully");
+//    return;
+//  }
+//}
+
 void loop() {
   float x, y, z;
-  
+
   if (gyroscopeAvailable()) {
     readGyroscope(x, y, z);
 
@@ -218,7 +325,7 @@ void loop() {
     Serial.println(z);
   }
 
-    if (accelerationAvailable()) {
+  if (accelerationAvailable()) {
     readAcceleration(x, y, z);
 
     Serial.print("Acceleration: ");
